70f4a615193d1f747a72833cf227733d29a2c3c5
saurabhpathak072 <saurabhspathak072@gmail.com>
2020-06-12 17:10:12 +0530

    second commt

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Home } from './views/home/Home';
-import { AddBoard } from './views/create-board/AddBoard';
-import { Header } from './components/header/Header';
-import { Board } from './views/board/Board';
-import SignUp from './views/signUp/SignUp';
-import Login from './views/login/Login';
-import { AuthProvider } from './context/Auth';
-import PrivateRoute from './common/guards/PrivateRoute';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faList, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
+import './App.css';
+
+import Boards from './../src/pages/Boards/Boards';
+import CreateBoard from './../src/pages/CreateBoard/CreateBoard';
+import Layout from './../src/pages/Layout/Layout';
+import Board from '../src/pages/Board/Board';
+
+library.add(faList, faTrashAlt);
 
 function App() {
+  let routes = (
+    <Switch>
+      <Route path="/createboard" component={CreateBoard}></Route>
+      <Route path="/board/:boardId" component={Board}></Route>
+      <Route path="/" component={Boards}></Route>
+      <Redirect to="/" />
+    </Switch>
+  );
+
   return (
-    <AuthProvider>
-      <Router>
-        <Header />
-        <Switch>
-          <PrivateRoute exact path="/" component={Home} />
-          <PrivateRoute path="/createboard" component={AddBoard} />
-          <PrivateRoute path="/board/:name" component={Board} />
-          <Route path="/signup" component={SignUp} />
-          <Route path="/login" component={Login} />
-          <Route exact path="*" component={Home} />
-        </Switch>
-      </Router>
-    </AuthProvider>
+    <div className="App">
+      <Layout>
+        {routes}
+      </Layout>
+    </div>
   );
 }
 
-export default App;
+export default withRouter(App);
